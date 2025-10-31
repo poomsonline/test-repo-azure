c28cec7300bdc77a4117faad2731c269ecca76c2
Sharjeel Rajput <sharjeel@truemeridian.com>
2025-10-31 19:44:42 +0500

    MINOR CHANGE IN NEXT CONFIG

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   output: 'export',
+   output: 'standalone', // DYNAMIC USE standalone OTHERWISE EXPORT
 };
 
 export default nextConfig;
